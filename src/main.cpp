5ea6e482d79aad41b1ef60ac88fd9ec1c07414a5
student <student@mini-06.local>
2022-03-16 11:29:02 +0100

    commit-4

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,14 @@
-#include "trygonometria.h"
+#ifdef USE_TRIGONOMETRY_DEGREE
+	#include "trygonometria.h"
+#else
+	#include <cmath>
+#endif
 #include <iostream>
 
-int main() { degreemath::sin(12); }
+int main() {
+#ifdef USE_TRIGONOMETRY_DEGREE
+	double result = degreemath::sin(45.0); 
+#else
+	double result = std::sin(M_PI/4.0); 
+#endif 
+}
